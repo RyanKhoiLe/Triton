295dd1fa089d6ceb884c6e246377e1e6a5f039f4
Ryan Khoi Le <ryanle16@mittymonarch.com>
2016-09-03 18:05:32 -0700

    made home page

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,57 +1,128 @@
-$(document).on("pagecreate", "#exhibit", function(){
+$(document).on("pagecreate", function(){
+
+  (function(){
+    var app = angular.module("mobileIndex", ["firebase"]);
+
+    app.config(function(){
+      var config = {
+        apiKey: "AIzaSyCH2GOoz3WtmgKC8gs9-P0eHk02-sX9vS0",
+        authDomain: "lunaspheretriton.firebaseapp.com",
+        databaseURL: "https://lunaspheretriton.firebaseio.com",
+        storageBucket: "lunaspheretriton.appspot.com",
+      };
+      firebase.initializeApp(config);
+    });
+
+    app.controller("mobileHome", ["$scope", "$firebaseArray", function($scope, $firebaseArray){
+      $scope.fbID = window.location.href.substring(window.location.href.indexOf("?") + 4);
+      $scope.title = "HELLO!";
+      // function makeExhibit(title, artist, id){
+      //   this.title = title;
+      //   this.artist = artist;
+      //   this.id = id;
+      // }
+      var ref = firebase.database().ref();
+      var exhibitRef = ref.child('exhibits');
+      // $scope.exhibitList = [];
+      // firebase.database().ref('exhibits/').on("child_added", function(snapshot){
+      //   var fbID = snapshot.val();
+      //   exhibitImage = fbID.exhibitImage;
+      //   title = fbID.title;
+      //   artist = fbID.artist;
+      //   uniqueId = snapshot.key;
+      //   exhibitCode = fbID.exhibitCode;
+      //   var exhibit = new makeExhibit(title, artist, uniqueId);
+      //   $scope.exhibitList.push(exhibit);
+      //   console.log("firebase is going...");
+      //   console.log($scope.exhibitList);
+      // });
+      // console.log($scope.exhibitList);
+      // console.log($scope.exhibitList[2]);
+      $scope.ExhibitList = $firebaseArray(exhibitRef);
+      console.log($scope.ExhibitList);
+    }]);
+
+    app.controller("slideInfo", ["$scope" ,"$firebaseArray", function($scope, $firebaseArray){
+      var ref = firebase.database().ref();
+      var thisUrl = window.location.href;
+      var idQ = thisUrl.indexOf('?');
+      var idStartAt = idQ + 4;
+      var fbID = thisUrl.substring(idStartAt);
+      console.log(fbID);
+      ref.child("exhibits").child(fbID).once('value').then(function(snapshot){
+        var exhibitData = snapshot.val();
+        var fieldArray = Object.keys(exhibitData);
+
+        console.log(fieldArray);
+        fieldArray.forEach(function(key){
+          console.log(key);
+          if(exhibitData[key] !== ''){
+            $scope[key] = exhibitData[key];
+            if(key === 'slides'){
+              console.log($scope.slides);
+              for(var i = 0; i < $scope.slides.length; i++){
+                if($scope.slides[i] === ""){
+                  $scope.slides.splice(i, 1);
+                  i--;
+                }
+              }
+            }
+            console.log($scope[key]);
+            // if(key==='symbols'){
+            //   $scope.symbols = exhibitData.symbols.split(';');
+            // }
+            // if(key==='exhibitImage'){
+            //   var storage = firebase.storage();
+            //   var storageRef = storage.ref();
+            //   var spaceRefAudio = storageRef.child(exhibitData[key]);
+            //   storageRef.child(exhibitData[key]).getDownloadURL().then(function(url){
+            //     var test = url;
+            //     $scope.exhibitImage = test;
+            //     console.log($scope.exhibitImage);
+            //   }).catch(function(error){
+            //   });
+            // }
+          }
+        });
+
+        console.log($scope.title);
+        //console.log(exhibitData);
+        console.log(exhibitData);
+        // var slider = document.getElementById("slider");
+        // //slider.setAttribute("class", "regular slider");
+        // for(var i = 0; i < $scope.slides.length; i++){
+        //   var slide = document.createElement("div");
+        //   slide.setAttribute("class", "slide");
+        //
+        //   var title = document.createElement("h1");
+        //   title.innerHTML = $scope.title;
+        //   var content = document.createElement("h3");
+        //   content.innerHTML = $scope.slides[i];
+        //   slide.appendChild(title);
+        //   slide.appendChild(content);
+        //   slider.appendChild(slide);
+        // }
+        // var details = document.createElement("div");
+        // details.setAttribute("class", "slide");
+        // var detailTitle = document.createElement("h1");
+        // detailTitle.innerHTML = $scope.title + ": Details";
+        // var detailContent = document.createElement("p");
+        // detailContent.innerHTML = " <b> Title: </b>"+ $scope.title+" <br> <b> Artist: </b>"+$scope.artist+" <br>"
+        // details.appendChild(detailTitle);
+        // details.appendChild(detailContent);
+        // slider.appendChild(details);
+        // //document.getElementById("angularContainer").appendChild(slider);
+
+        $scope.$apply();
+
+      });
+    }]);
+  })();
+
   $(".regular").slick({
     dots: true,
     infinite: true,
+    focusOnSelect: false
   });
+
 });
-
-(function(){
-  var app = angular.module("mobileIndex", ["firebase"]);
-
-  app.config(function(){
-    var config = {
-      apiKey: "AIzaSyCH2GOoz3WtmgKC8gs9-P0eHk02-sX9vS0",
-      authDomain: "lunaspheretriton.firebaseapp.com",
-      databaseURL: "https://lunaspheretriton.firebaseio.com",
-      storageBucket: "lunaspheretriton.appspot.com",
-    };
-    firebase.initializeApp(config);
-  });
-
-  app.controller("slideInfo", ["$scope" ,"$firebaseArray", function($scope, $firebaseArray){
-    var ref = firebase.database().ref();
-    ref.child("exhibits").child("-KQgNAb_IcFTjJV6YpL6").once('value').then(function(snapshot){
-      var exhibitData = snapshot.val();
-      var fieldArray = Object.keys(exhibitData);
-
-      console.log(fieldArray);
-      fieldArray.forEach(function(key){
-        console.log(key);
-        if(exhibitData[key] !== ''){
-          $scope[key] = exhibitData[key];
-          console.log($scope[key]);
-          // if(key==='symbols'){
-          //   $scope.symbols = exhibitData.symbols.split(';');
-          // }
-          // if(key==='exhibitImage'){
-          //   var storage = firebase.storage();
-          //   var storageRef = storage.ref();
-          //   var spaceRefAudio = storageRef.child(exhibitData[key]);
-          //   storageRef.child(exhibitData[key]).getDownloadURL().then(function(url){
-          //     var test = url;
-          //     $scope.exhibitImage = test;
-          //     console.log($scope.exhibitImage);
-          //   }).catch(function(error){
-          //   });
-          // }
-        }
-      });
-
-      console.log($scope.title);
-      //console.log(exhibitData);
-      console.log(exhibitData);
-      $scope.$apply();
-
-    });
-  }]);
-})();
--- scripts/pagecreator.js
+++ scripts/pagecreator.js
@@ -1,18 +1,148 @@
-var app = angular.module("pcApp", ["firebase"]);
-app.config(function(){
-  var config = {
-    apiKey: "AIzaSyCH2GOoz3WtmgKC8gs9-P0eHk02-sX9vS0",
-    authDomain: "lunaspheretriton.firebaseapp.com",
-    databaseURL: "https://lunaspheretriton.firebaseio.com",
-    storageBucket: "lunaspheretriton.appspot.com",
-  };
-  firebase.initializeApp(config);
-});
+(function(){
+  var app = angular.module("pcApp", ["firebase"]);
+  app.config(function(){
+    var config = {
+      apiKey: "AIzaSyCH2GOoz3WtmgKC8gs9-P0eHk02-sX9vS0",
+      authDomain: "lunaspheretriton.firebaseapp.com",
+      databaseURL: "https://lunaspheretriton.firebaseio.com",
+      storageBucket: "lunaspheretriton.appspot.com",
+    };
+    firebase.initializeApp(config);
+  });
 
-app.controller("createNewPage", [$scope]){
-  $scope.firebase = firebase.database();
-  $scope.title = title;
-  $scope.artist = artist;
-  $scope.year = year;
-  $scope.genre;
-}
+  app.controller("createNewPage", ["$scope", function($scope){
+    $scope.numberOfSlides = 3;
+    $scope.oneImage = true;
+    $scope.exhibitSelect = "";
+    $scope.details = true;
+    $scope.slideIds = [];
+    $scope.slideValues = [];
+    $scope.year = "";
+    $scope.artist = "";
+    $scope.videos = "";
+    $scope.title = "";
+    $scope.media = "";
+    $scope.genre = "";
+    $scope.exhibitCode = "";
+    $scope.exhibitAudio = "";
+    $scope.exhibitImage = "";
+    $scope.showModal = false;
+    $scope.inputtedExhibit = "";
+
+    for(var i = 0; i < 10; i++){
+      $scope.slideIds.push(false);
+      $scope.slideValues.push("");
+    }
+    for(var i = 0; i < $scope.numberOfSlides; i++){
+      $scope.slideIds[i] = true;
+    }
+    console.log($scope.slideIds);
+      // document.getElementById("numberOfSlides").onchange = function(){
+      //   console.log($scope.numberOfSlides);
+      //   //TODO make slides using ng-repeat
+      //   for(var i = 0; i <$scope.slideIds.length; i++){
+      //     $scope.slideIds[i] = false;
+      //   }
+      //   for(var i = 0; i < $scope.numberOfSlides; i++){
+      //     $scope.slideIds[i] = true;
+      //   }
+      //   $scope.$apply();
+      // };
+      document.getElementById("oneImage").onchange = function(evt){
+        console.log($scope.oneImage);
+      }
+      $scope.checkExhibit = function(){
+        console.log($scope.exhibitSelect);
+        //console.log(document.getElementById("exhibitSelected").textContent);
+        if($scope.exhibitSelect === "New"){
+          console.log("NEW IS SELECTED");
+          $scope.inputExhibit = true;
+        }
+        else{
+          $scope.inputExhibit = false;
+        }
+        //$scope.$apply();
+      }
+      $scope.createNewPage = function(){
+        // evt.preventDefault();
+        console.log("Exhibit:" + $scope.exhibitSelect);
+        console.log("This database is managed by Khoi Le, 2016");
+        var title = $scope.title,
+        artist = $scope.artist,
+        year = $scope.year,
+        genre = $scope.genre,
+        media = $scope.media,
+        //exhibitImage = $scope.exhibitImage,
+        //videos = $scope.videos,
+        //timeStamp = getTimeStamp(),
+        //exhibitAudio = $scope.exhibitAudio,
+        exhibitCode = $scope.exhibitCode,
+        exhibit = $scope.exhibitSelect,
+        slides = [];
+        if($scope.inputExhibit === true){
+          exhibit = document.getElementById("inputtedExhibit").value;
+        }
+        for(var i = 0; i < 10; i++){
+          if($scope.slideIds[i]){
+            console.log("pushing to slides");
+              slides = $scope.slideValues;
+          }
+          else{
+            break;
+          }
+        }
+        console.log(slides);
+        if(title && exhibitCode && slides){
+          var key = firebase.database().ref('exhibits/').push({
+            title: title,
+            artist: artist,
+            year: year,
+            genre: genre,
+            media: media,
+            //exhibitImage: exhibitImage,
+            //videos: videos,
+            //timeStamp: timeStamp,
+            views: 0,
+            exhibitCode: exhibitCode,
+            slides: slides,
+            exhibit: exhibit
+            //exhibitAudio: exhibitAudio
+          }, function(){
+            console.log("successfully written to khoi le's firebase");
+            $scope.showModal = true;
+            $scope.$apply();
+          }).key;
+        }
+        else{
+          alert('Lunasphere wants to push your best content. Please enter a title, artist, and information for the first slide');
+        }
+      }
+      $scope.makeAnother = function (){
+        window.location.href = 'pagecreatorMobile.html';
+      }
+      $scope.sendToExhibit = function (){
+        window.location.href = 'exhibit.html?id=' + fbID;
+      }
+      $scope.editExhibit = function (){
+        window.location.href = 'editexhibit.html?id=' + fbID;
+      }
+      $scope.goToExplorer = function (){
+        window.location.href = 'databaseExplorer.html';
+      }
+      function getTimeStamp(){
+        var now = new Date();
+        var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+        var time = [now.getHours(), now.getMinutes()];
+        var suffix = (time[0] < 12) ? "AM" : "PM";
+        time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
+
+        for(var i = 1; i < 3; i++){
+          if(time[i] < 10) {
+            time[i] = "0" + time[i];
+          }
+        }
+
+        return date.join("/") + ", " + time.join(":") + " " + suffix;
+      }
+  }]);
+})();
